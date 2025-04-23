39928fd9d6cd6dc241139a2a499de60530af84db
RomanB54 <r.barsukov@mgmnt.ru>
2025-04-23 14:03:20 +0300

    env installed

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import globals from 'globals';
+import jest from 'eslint-plugin-jest';
+import eslintRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  eslintRecommended,
+  tseslint.configs.recommended,
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      'prefer-const': 'warn',
+      'no-unused-vars': 'off',
+      semi: 'error',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+    },
+  },
+  {
+    files: ['srcjs/*.js'],
+    ...jest.configs['flat/recommended'],
+  },
+);
